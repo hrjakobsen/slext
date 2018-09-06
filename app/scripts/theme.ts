ae6854328e0d3124076a0d6978ee5131eaab0d10
Mathias Jakobsen <mathias@jbcoding.com>
2018-09-06 22:36:25 +0200

    Add new page element to theming and add new themes based on overleaf design

--- app/scripts/theme.ts
+++ app/scripts/theme.ts
@@ -21,6 +21,8 @@ interface ThemeStructure {
     scrollbarThumbColor: string;
     buttonColorHover: string;
     buttonBorder: string;
+    actionColor: string;
+    headerColor: string;
 }
 
 interface Theme {
@@ -35,11 +37,13 @@ export class ThemeModule {
             name: 'Dark',
             theme: {
                 accentColor: '#a93529',
+                actionColor: '#a93529',
                 accentColorHover: '#6b221a',
                 accentColorActive: 'white',
                 textColor: '#a4a4a4',
                 textColorActive: 'white',
                 backgroundColor: '#333',
+                headerColor: '#333',
                 fileColor: '#a4a4a4',
                 fileColorHover: '#a93529',
                 scrollbarBackgroundColor: '#313131',
@@ -53,10 +57,12 @@ export class ThemeModule {
             theme: {
                 accentColor: '#a93529',
                 accentColorHover: '#6b221a',
+                actionColor: '#a93529',
                 accentColorActive: 'white',
                 textColor: '#808080',
                 textColorActive: '#202020',
                 backgroundColor: 'white',
+                headerColor: 'white',
                 fileColor: '#404040',
                 fileColorHover: '#a93529',
                 scrollbarBackgroundColor: '#eee',
@@ -69,17 +75,57 @@ export class ThemeModule {
             name: 'Blue',
             theme: {
                 accentColor: '#609090',
+                actionColor: '#609090',
                 accentColorHover: '#add3e1',
                 accentColorActive: '#839496',
                 textColor: '#809090',
                 textColorActive: '#ccffff',
                 backgroundColor: '#002b36',
+                headerColor: '#002b36',
                 fileColor: '#586e75',
                 fileColorHover: '#add3e1',
                 scrollbarBackgroundColor: '#073642',
                 scrollbarThumbColor: '#586e75',
                 buttonBorder: '#cfcfcf',
                 buttonColorHover: '#657b83'
+            }
+        },
+        {
+            name: 'Overleaf-light',
+            theme: {
+                accentColor: '#4f9c45',
+                actionColor: '#727E91',
+                accentColorHover: '#418038',
+                accentColorActive: 'white',
+                textColor: '#808080',
+                textColorActive: '#202020',
+                backgroundColor: 'white',
+                headerColor: 'white',
+                fileColor: '#9DA7B7',
+                fileColorHover: '#9DA7B7',
+                scrollbarBackgroundColor: '#eee',
+                scrollbarThumbColor: '#ccc',
+                buttonBorder: '#cfcfcf',
+                buttonColorHover: 'rgba(0,0,0,0.1)'
+            }
+        },
+        {
+            name: 'Overleaf-dark',
+            theme: {
+                accentColor: '#4f9c45',
+                actionColor: 'white',
+                accentColorHover: 'white',
+                accentColorActive: 'white',
+                textColor: '#ccc',
+                textColorActive: 'white',
+                backgroundColor: '#455265',
+                headerColor: '#1E2530',
+                fileColor: '#ddd',
+                fileColorHover: '#ddd',
+                scrollbarBackgroundColor: '#eee',
+                scrollbarThumbColor: '#ccc',
+                buttonBorder: '#cfcfcf',
+                buttonColorHover: 'rgba(0,0,0,0.1)'
             }
         }
     ];
