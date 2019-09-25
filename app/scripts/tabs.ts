36a5f1fa0bc227345ab68bd29cb8da2e86316f32
Mathias Jakobsen <mathias@jbcoding.com>
2019-09-25 08:03:58 +0200

    Update extension to fix firefox add-on

--- app/scripts/tabs.ts
+++ app/scripts/tabs.ts
@@ -139,7 +139,7 @@ export class TabModule {
             }
 
             if (self._tabs.includes(clickedTab)) {
-                $(clickedTab.file.handle)[0].click();
+                $(clickedTab.file.handle).first().click();
             }
         });
 
